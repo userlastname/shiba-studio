8c4f12fc6a0e5718d568ad134ff38481e9114fda
Daniel Palmer <dan@tribefire.co>
2019-11-19 11:34:43 +1100

    netlify form test

--- src/components/poke-form.vue
+++ src/components/poke-form.vue
@@ -1,17 +1,20 @@
 <template>
   <section>
-    <form class="max-width flex flex-col">
+    <form method="post" name="poke" class="max-width flex flex-col" netlify>
       <h3 class="text-2xl mb-2 px-4 md:px-8 text-center">
         Poke us and we’ll get in touch!
       </h3>
 
-      <label for="name">Your name...</label>
-      <input type="text" name="name" id="name" required />
+      <label for="name"
+        >Your name... <input type="text" name="name" id="name" required
+      /></label>
 
-      <label for="email" class="mt-6">Your email address...</label>
-      <input type="email" name="email" id="email" required />
+      <label for="email" class="mt-6"
+        >Your email address...
+        <input type="email" name="email" id="email" required
+      /></label>
 
-      <button class="bg-yellow text-lg mt-4 w-full text-center">
+      <button type="submit" class="bg-yellow text-lg mt-4 w-full text-center">
         Poke Shiba Studio
       </button>
     </form>
